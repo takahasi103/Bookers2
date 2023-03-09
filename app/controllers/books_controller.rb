28ea7b98cf7cd1e61e7df811fab7e35ae5adc98f
takahasi103 <124013519+takahasi103@users.noreply.github.com>
2023-03-09 12:24:42 +0000

    [Save] user_validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "successfully book create"
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -27,9 +28,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully book update"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end 
   
   def destroy
